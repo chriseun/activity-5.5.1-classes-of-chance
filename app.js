9a9ed5821f3411f097b61420561e0c3c8c831a32
Chris Eun <eunchristopher@gmail.com>
2022-11-21 19:37:05 -0800

    Javascript example using Class

--- app.js
+++ app.js
@@ -1,30 +1,43 @@
 class Casino {
-    // Write code here
+    constructor(name, isFakeCoin){
+        this.name = name;
+        this.timesPlayed = 0;
+        this.isFakeCoin = isFakeCoin;
+    }
+    playGame(betAmout){
+        // this.timesPlayed++
+        if (Math.random() <= 0.5 || this.isFakeCoin === true){
+            console.log(`${this.name} wins`)
+        }
+        else {
+            console.log(`player wins! You won ${betAmout * (this.timesPlayed + 1)}`)
+        }
+    }
+
 };
 
 // TESTS
-const myCasino = new Casino("HackerU Casino");
-console.log(myCasino);
-myCasino.playGame(5);
-myCasino.playGame(15);
-myCasino.playGame(25);
-myCasino.playGame(35);
+// const myCasino = new Casino("HackerU Casino");
+// console.log(myCasino);
+// myCasino.playGame(5);
+// myCasino.playGame(15);
+// myCasino.playGame(25);
+// myCasino.playGame(35);
 
 // BONUS TESTS
-/*
+
 const myBonusCasino = new Casino("HackerU Bonus Casino", true);
 console.log(myBonusCasino);
 myBonusCasino.playGame(5);
 myBonusCasino.playGame(15);
 myBonusCasino.playGame(25);
 myBonusCasino.playGame(35);
-*/
+
 
 // Extra BONUS TESTS
-/*
-const myExtraBonusCasino = new Casino("HackerU Extra Bonus Casino", false);
-console.log(myExtraBonusCasino);
-myExtraBonusCasino.rollDie(6);
-myExtraBonusCasino.rollDie(20);
-myExtraBonusCasino.rollDie(100);
-*/
+
+// const myExtraBonusCasino = new Casino("HackerU Extra Bonus Casino", false);
+// console.log(myExtraBonusCasino);
+// myExtraBonusCasino.rollDie(6);
+// myExtraBonusCasino.rollDie(20);
+// myExtraBonusCasino.rollDie(100);
